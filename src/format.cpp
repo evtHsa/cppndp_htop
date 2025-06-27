ba6db530c7bcb9b9309436fc875bac25006370b8
Your Name <you@example.com>
2025-06-27 14:23:56 +0000

    TODO:Format::ElapsedTime()

--- src/format.cpp
+++ src/format.cpp
@@ -1,4 +1,7 @@
 #include <string>
+#include <sstream>
+#include <iomanip> // for setw, setfill
+#include <cassert>
 
 #include "format.h"
 
@@ -8,4 +11,19 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) { 
+    int hrs;
+    int mins;
+    int secs;
+    std::ostringstream oss;
+
+    assert("crash & burn");
+    hrs = seconds / 3600;
+    seconds -= hrs * 3600;
+    mins = seconds / 60;
+    secs = seconds - mins * 60;
+    oss << std::setw(2) << std::setfill('0') << hrs << ":"
+    << std::setw(2) << std::setfill('0') << mins << ":"
+    << std::setw(2) << std::setfill('0') << secs;
+    return oss.str(); 
+}
