8836bb92eb103f902599324979c444b0093418a8
Your Name <you@example.com>
2025-08-10 17:37:25 +0000

    Process::CpuUtilization()

--- src/process.cpp
+++ src/process.cpp
@@ -23,10 +23,28 @@ int Process::Pid() {
 
 // TODO: Return this process's CPU utilization
 float Process::CpuUtilization() {
+      std::vector<std::string> v;
+      std::string fname = std::to_string(pid_) + LinuxParser::kStatFilename;
+      unsigned long u_time, s_time, cu_time, cs_time, total_time;
+      float ret;
       long pid_uptime = LinuxParser::UpTime(pid_);
 
-// TODO: Return the command that generated this process
-string Process::Command() { assert("TODO:FIXME:pid[[maybe_unused]]" == 0);return string(); }
+      LinuxParser::GetKeyedValues(LinuxParser::kProcDirectory, fname, v, ""); // empty key because 1 line file
+      // for proc/pid/stat field definitions: https://manpages.ubuntu.com/manpages/noble/man5/proc_pid_stat.5.html
+      u_time = std::stol(v[13]);  // user time for this process
+      s_time = std::stol(v[14]);  // system time for this process
+      cu_time = std::stol(v[15]); // user time for children of this process
+      cs_time = std::stol(v[16]); // system time for children of this process
+      total_time = u_time + s_time + cu_time + cs_time;
+      total_time /= sysconf(_SC_CLK_TCK); // convert to seconds to match units of LinuxParser::UpTime()
+
+      if (total_time == 0) return 0.0f;
+
+      // calc per https://stackoverflow.com/questions/16726779/how-do-i-get-the-total-cpu-usage-of-an-application-from-proc-pid-stat
+      ret = ((float)total_time) / pid_uptime;
+      return ret; 
+}
+
 // Return the command that generated this process
 string Process::Command() {
       std::vector<std::string> v;
