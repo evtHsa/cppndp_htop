13374b5d3b517e4dca52eeb2820d693ecb2826d5
Your Name <you@example.com>
2025-08-29 19:26:24 +0000

    Report days in uptime as more aesthetic than huge numbers of hours

--- src/format.cpp
+++ src/format.cpp
@@ -10,17 +10,20 @@ using std::string;
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
 string Format::ElapsedTime(long seconds) { 
-    int hrs;
-    int mins;
-    int secs;
+    int days, hrs, mins, secs;
     std::ostringstream oss;
 
+    string ret;
+    days = seconds / (3600 * 24);
+    seconds -= days * 3600 * 24;
     hrs = seconds / 3600;
     seconds -= hrs * 3600;
     mins = seconds / 60;
     secs = seconds - mins * 60;
-    oss << std::setw(2) << std::setfill('0') << hrs << ":"
+    oss << std::setw(3) << days << " days, "
+      << std::setw(2) << std::setfill('0') << hrs << ":"
     << std::setw(2) << std::setfill('0') << mins << ":"
     << std::setw(2) << std::setfill('0') << secs;
-    return oss.str(); 
+    ret = oss.str();
+    return ret; 
 }
